93ecba6bee97e2a70990b1d38ee7f64d2d5bad9e
xxboomstick <ddchang1iz@gmail.com>
2024-10-03 18:21:20 -0400

    add tickers to cleaned-fligth

--- flightandcrsp/mutate.py
+++ flightandcrsp/mutate.py
@@ -3,10 +3,15 @@ import pandas as pd
 crsp = pd.read_csv('../cleaned-crsp.csv')
 flight = pd.read_csv('../cleaned-flight.csv')
 merged = pd.read_csv('merged_flight_data.csv')
+compared = pd.read_csv('compared.csv')
 
-print(len(merged))
-print(len(merged.dropna()))
-print(len(merged.dropna()) / len(merged))
+
+tickers = {key: value for key,
+           value in zip(compared['Company'], compared['Ticker'])}
+
+flight['Ticker'] = flight['Company'].apply(
+    lambda c: tick if (tick := tickers.get(c)) is not None else "None")
 
 # crsp.to_csv("../cleaned-crsp.csv", index=False)
-# flight.to_csv("../cleaned-flight.csv", index=False)
+flight.to_csv("../cleaned-flight.csv", index=False)
+# compared.to_csv("compared.csv", index=False)
