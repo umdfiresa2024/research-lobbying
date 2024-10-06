c220824b6b821c84517d2ed899cfb9b73fd75ed7
Daven <dchang1iz@gmail.com>
2024-10-05 23:16:39 -0400

    da

--- flightandcrsp/mutate.py
+++ flightandcrsp/mutate.py
@@ -1,17 +1,14 @@
 import pandas as pd
 
-crsp = pd.read_csv('../cleaned-crsp.csv')
-flight = pd.read_csv('../cleaned-flight.csv')
-merged = pd.read_csv('merged_flight_data.csv')
-compared = pd.read_csv('compared.csv')
+crsp = pd.read_csv('../200908-201012CRSP.csv')
+flight = pd.read_csv('../flight_2010.csv')
+final = pd.read_csv('final.csv')
 
 
-tickers = {key: value for key,
-           value in zip(compared['Company'], compared['Ticker'])}
-
-flight['Ticker'] = flight['Company'].apply(
-    lambda c: tick if (tick := tickers.get(c)) is not None else "None")
+tick2naics = {k: v for k, v in zip(crsp['TICKER'], crsp['NAICS'])}
+final['naics'] = [tick2naics.get(k) for k in final['Ticker']]
+print(final)
 
 # crsp.to_csv("../cleaned-crsp.csv", index=False)
-flight.to_csv("../cleaned-flight.csv", index=False)
-# compared.to_csv("compared.csv", index=False)
+# flight.to_csv("../cleaned-flight.csv", index=False)
+final.to_csv("final.csv", index=True, index_label='Index')
