b2ed7cf715d97bf0d3da4ee99da23e3408ec9673
Daven <dchang1iz@gmail.com>
2024-10-14 17:02:05 -0400

    match tickers to meng companies

--- flightandcrsp/mutate.py
+++ flightandcrsp/mutate.py
@@ -1,14 +1,19 @@
 import pandas as pd
+import numpy as np
 
 crsp = pd.read_csv('../200908-201012CRSP.csv')
 flight = pd.read_csv('../flight_2010.csv')
 final = pd.read_csv('final.csv')
+test = pd.read_csv('../meng_tickers.csv')
+test2 = pd.read_csv('../meng_tickers2.csv')
 
-
-tick2naics = {k: v for k, v in zip(crsp['TICKER'], crsp['NAICS'])}
-final['naics'] = [tick2naics.get(k) for k in final['Ticker']]
-print(final)
+for i in range(len(test)):
+    if test.iloc[i]["Company"] != test2.iloc[i]["Company"]:
+        print("Different name")
+    elif test.iloc[i]["Ticker"] != test2.iloc[i]["Ticker"]:
+        print(
+            f"{test.iloc[i]['Company']:<20} {test.iloc[i]['Ticker']:<10} {test2.iloc[i]['Ticker']}")
 
 # crsp.to_csv("../cleaned-crsp.csv", index=False)
 # flight.to_csv("../cleaned-flight.csv", index=False)
-final.to_csv("final.csv", index=True, index_label='Index')
+# final.to_csv("final.csv", index=True, index_label='Index')
