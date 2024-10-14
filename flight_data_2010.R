422439a7713ecfcd73fbf5493950b4325ac7d443
Anya Jain <anyajain@wireless-10-104-167-223.umd.edu>
2024-10-14 14:56:18 -0400

    Ticker Code Added

--- flight_data_2010.R
+++ flight_data_2010.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(httr)
+library(jsonlite)
 
 # Read the CSV file
 combined_data <- read.csv("flight_2010.csv")
@@ -49,12 +51,75 @@ data_long6 <- data_long5 %>%
   ungroup() 
 
 # View the summarized data
-print(data_long)
 
-# Summarize GHG quantities by parent company
-data_summary <- data_long %>%
-  group_by(PARENT_COMPANY) %>%
-  summarize(GHG_sum = sum(GHG_sum, na.rm = TRUE), .groups = 'drop')
+companies<-as.vector(data_long6$PARENT_COMPANY)
+companies
 
-# View the final summary
-print(data_summary)
+get_sec <- function(i) {
+  tryCatch(
+    #try to do this
+    {
+      name<- companies[i]
+      api<-"0b2ca623403335d8c3d2e75932b87231a96f7fa6e3d7e68f569edf05fd614e97"
+      url<-paste0("https://api.sec-api.io/mapping/name/", name, "?token=", api)
+      
+      filename<-"flight_sec/file.json"
+      GET(url,write_disk(filename, overwrite = TRUE))
+      
+      data <- fromJSON("flight_sec/file.json")
+      
+      # if data doesn't have a ticker for a company, make that cell NA
+      if (!is.null(data$ticker)) {
+        ticker <- data$ticker[1]
+        print(data$ticker[1])
+      } else {
+        ticker <- NA
+      }
+      
+    },
+    #if an error occurs, tell me the error
+    error=function(e) {
+      message('An Error Occurred')
+      print(e)
+    },
+    #if a warning occurs, tell me the warning
+    warning=function(w) {
+      message('A Warning Occurred')
+      print(w)
+      return(NA)
+    }
+  )
+}
+
+# function to look up the ticker symbols from sec data and store the data of companies and symbols as data
+get_sec <- function(i) {
+  name<- companies[i]
+  api<-"0b2ca623403335d8c3d2e75932b87231a96f7fa6e3d7e68f569edf05fd614e97"
+  url<-paste0("https://api.sec-api.io/mapping/name/", name, "?token=", api)
+  
+  filename<-"flight_sec/file.json"
+  GET(url,write_disk(filename, overwrite = TRUE))
+  
+  data <- fromJSON("flight_sec/file.json")
+  
+  # if data doesn't have a ticker for a company, make that cell NA
+  if (!is.null(data$ticker)) {
+    ticker <- data$ticker[1]
+    print(data$ticker[1])
+  } else {
+    ticker <- NA
+  }
+} 
+
+# make a list of the tickers for all companies with look
+tickers <- c()
+for (i in 1:5) {
+  print(i)
+  ticker <- get_sec(i)
+  tickers<-rbind(tickers, ticker)
+}
+
+# create dataframe of tickers
+tickers_df <- data.frame(ticker = tickers)
+
+#write.csv(tickers_df, "sec_api/tickers_df_1_5.csv", row.names = F)
